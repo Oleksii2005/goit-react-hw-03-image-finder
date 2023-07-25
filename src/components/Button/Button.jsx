ec99da2cb0dd50300832e69950d0949600a4d497
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-07-25 22:17:50 +0200

    centered button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import css from '../Button/Button.module.css';
 export const Button = ({ onClick, showButton }) => {
   return showButton ? (
-    <button type="button" onClick={onClick} className={css.button}>
+    <button type="button" onClick={onClick} className={css.Button}>
       Load more...
     </button>
   ) : null;
